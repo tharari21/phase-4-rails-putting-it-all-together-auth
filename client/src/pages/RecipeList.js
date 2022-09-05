e25e76c9581dac7353b900a43dec0e9fdec5bbd4
Tomer Harari <tharari21@gmail.com>
2022-09-05 18:06:47 -0400

    commit tree 18a8b920ffb8f048d92145a8a27f87175ef005a3

--- client/src/pages/RecipeList.js
+++ client/src/pages/RecipeList.js
@@ -10,7 +10,10 @@ function RecipeList() {
   useEffect(() => {
     fetch("/recipes")
       .then((r) => r.json())
-      .then(setRecipes);
+      .then((data) => {
+        console.log(data)
+        setRecipes(data)
+});
   }, []);
 
   return (
